65db22b38e0f190dabb63f51d14e942e8c6f27b0
BjornMelgaard <melgaardbjorn@gmail.com>
2017-08-28 10:39:53 +0300

    with NonstandartConfigurationJoiner

--- types/webpack-merge/index.d.ts
+++ types/webpack-merge/index.d.ts
@@ -1,14 +1,14 @@
 // Type definitions for webpack-merge
 // Project: https://github.com/survivejs/webpack-merge
 // Definitions by: Simon Hartcher <https://github.com/deevus>
+//                 Sergey Homa <https://github.com/BjornMelgaard>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-///<reference types="webpack" />
+import { Configuration } from "webpack";
 
-declare module "webpack-merge" {
-  import { Configuration } from "webpack";
+export = Merge
 
-  // interfaces
+declare namespace Merge {
   interface ConfigurationJoiner {
     (...configuration: Configuration[]): Configuration
     (configuration: Configuration[]): Configuration
@@ -16,60 +16,59 @@ declare module "webpack-merge" {
 
   type CustomizeFunc = (a: any, b: any, key: string) => any
   type CustomizationObj = {
-    customizeArray:  CustomizeFunc,
-    customizeObject: CustomizeFunc
+    customizeArray?:  CustomizeFunc,
+    customizeObject?: CustomizeFunc
   }
 
   type StrategyType = 'prepend' | 'append' | 'replace'
   type StrategyMap = { [feild: string]: StrategyType }
 
-  // core
-  interface WebpackMerge {
-    /**
-    * Merge multiple webpack configurations into one.
+  /**
+  * Unique merge strategy
+  */
+  const unique: (
+    key: string,
+    uniques: string[],
+    getter: (a: any) => any
+  ) => any
+
+  /**
+    * Merging with a customizable merge strategy
     */
-    (...configuration: Configuration[]): Configuration
+  const strategy: (options: StrategyMap) => ConfigurationJoiner
 
-    /**
-    * Merge array of webpack configurations into one.
+  /**
+    * Merging with a customizable smart merge strategy
     */
-    (configuration: Configuration[]): Configuration
+  const smartStrategy: (options: StrategyMap) => ConfigurationJoiner
 
-    /**
-    * Merge multiple webpack configurations into one with custom behavior.
-    */
-    (customizationObj: CustomizationObj): ConfigurationJoiner
-
-    /**
-    * Unique merge strategy
-    */
-    unique: (
-      key: string,
-      uniques: string[],
-      getter: (a: any) => any
-    ) => any
-
-    /**
-     * Merging with a customizable merge strategy
-     */
-    strategy: (options: StrategyMap) => ConfigurationJoiner
-
-    /**
-     * Merging with a customizable smart merge strategy
-     */
-    smartStrategy: (options: StrategyMap) => ConfigurationJoiner
-
-    /**
-     * Merging with a smart merge strategy
-     */
-    smart: ConfigurationJoiner
-
-    /**
-     * Merging with a multiple merge strategy
-     */
-    multiple: ConfigurationJoiner
+  interface NonstandartConfigurationJoiner {
+    (...configuration: any[]): Configuration
+    (configuration: any[]): Configuration
   }
 
-  const merge: WebpackMerge;
-  export = merge;
+  /**
+    * Merging with a smart merge strategy
+    */
+  const smart: NonstandartConfigurationJoiner
+
+  /**
+    * Merging with a multiple merge strategy
+    */
+  const multiple: NonstandartConfigurationJoiner
 }
+
+/**
+* Merge multiple webpack configurations into one.
+*/
+declare function Merge(...configuration: Configuration[]): Configuration
+
+/**
+* Merge array of webpack configurations into one.
+*/
+declare function Merge(configuration: Configuration[]): Configuration
+
+/**
+* Merge multiple webpack configurations into one with custom behavior.
+*/
+declare function Merge(customizationObj: Merge.CustomizationObj): Merge.ConfigurationJoiner
--- types/webpack-merge/webpack-merge-tests.ts
+++ types/webpack-merge/webpack-merge-tests.ts
@@ -1,4 +1,5 @@
-import webpackMerge, { CustomizationObj } from "webpack-merge"
+import * as webpackMerge from "webpack-merge"
+import * as webpack from "webpack";
 import { Configuration } from "webpack";
 
 // fixtures
@@ -8,9 +9,9 @@ const a: Configuration = {
 const b: Configuration = {
   devtool: "source-map"
 }
+const ab = [a, b]
 
-// merge
-const bogusCustomizer: CustomizationObj = {
+const bogusCustomizer: webpackMerge.CustomizationObj = {
   customizeArray(a, b, key) {
     return undefined;
   },
@@ -19,5 +20,84 @@ const bogusCustomizer: CustomizationObj = {
   }
 }
 
-var output = webpackMerge.merge(bogusCustomizer)(a, b);
+// Default API
+var output = webpackMerge(a, b);
+var output = webpackMerge(ab);
 
+// Customizing array/object behavior
+var output = webpackMerge(bogusCustomizer)(a, b);
+
+// unique
+var output = webpackMerge({
+  customizeArray: webpackMerge.unique(
+    'plugins',
+    ['HotModuleReplacementPlugin'],
+    plugin => plugin.constructor && plugin.constructor.name
+  )
+})({
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}, {
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
+
+// strategy
+var output = webpackMerge.strategy(
+  {
+    entry: 'prepend', // or 'replace', defaults to 'append'
+    'module.loaders': 'prepend'
+  }
+)(a, b);
+
+// smartStrategy
+var output = webpackMerge.smartStrategy(
+  {
+    entry: 'prepend', // or 'replace'
+    'module.loaders': 'prepend'
+  }
+)(a, b);
+
+// smart
+var output = webpackMerge.smart({
+  loaders: [{
+    test: /\.js$/,
+    loader: 'babel'
+  }]
+}, {
+  loaders: [{
+    test: /\.js$/,
+    loader: 'coffee'
+  }]
+});
+
+// multiple
+var path = 'asdf'
+var baseConfig = {
+  server: {
+    target: 'node',
+    output: {
+      path: path,
+      filename: 'lib.node.js'
+    }
+  },
+  client: {
+    output: {
+      path: path,
+      filename: 'lib.js'
+    }
+  }
+};
+
+var production = {
+  client: {
+    output: {
+      path: path,
+      filename: '[name].[hash].js'
+    }
+  }
+}
+
+var output = webpackMerge.multiple(baseConfig, production)
